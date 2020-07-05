c146a76bb9f4476b3bddbc752314f0c38438cd28
John Purcell <john.purcell@caveofprogramming.com>
2020-07-05 14:44:31 +0200

    01090 Creating a Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import Book from './Book'
+import './App.css';
 
 function App() {
 
-  const greeting = <div>Hello world!!!</div>;
-
   return (
     <div className="App">
-      {greeting}
+      <Book />
     </div>
   );
 }
---
+++ client/src/Book.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Book.css';
+
+function Book() {
+    return (
+        <div id='book-message'>Hello I'm a book!</div>
+    );
+}
+
+export default Book;
