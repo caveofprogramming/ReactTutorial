98e0a56341b6cdbe1cbb068fb21456fb26f81680
John Purcell <john.purcell@caveofprogramming.com>
2020-07-06 12:50:49 +0200

    02060 A Properties File

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+const db = {
+ 'user': 'books',
+ 'password': 'hello',   
+}
+
+module.exports = db;
--- server/routes/books.js
+++ server/routes/books.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+const db = require('../config/db');
+
+console.log(db.user, db.password);
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('books here!');
