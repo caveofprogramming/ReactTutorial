a96efa15f55a026200a4ca6f1877b0d89b92ac7c
John Purcell <john.purcell@caveofprogramming.com>
2020-07-11 15:42:02 +0200

    03020 Environment Variables

--- client/src/Book.js
+++ client/src/Book.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './Book.css';
 
 function Book() {
+
+    console.log('server url', process.env.REACT_APP_SERVER_URL);
+
     return (
         <div id='book-message'>Hello I'm a book!</div>
     );
