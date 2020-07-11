da6fade504b192950e8400b5c212a3496388246f
John Purcell <john.purcell@caveofprogramming.com>
2020-07-11 16:43:25 +0200

    03050 State

--- client/src/BookLibrary.js
+++ client/src/BookLibrary.js
@@ -3,14 +3,25 @@ import axios from 'axios';
 
 class BookLibrary extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        console.log('book library')
+
+        this.state = {
+            books: [],
+        };
+    }
+
     componentDidMount() {
 
         axios(process.env.REACT_APP_SERVER_URL)
-            .then(result => console.log(result))
+            .then(result => this.setState({ books: result.data }))
             .catch(error => console.log(error));
     }
 
     render() {
+        console.log('render', this.state.books);
         return ( <div>Library</div>);
     }
 }
