389757cd1ab27a7cffbdb7bb5496d90e190c0672
John Purcell <john.purcell@caveofprogramming.com>
2020-07-12 12:31:25 +0200

    03080 Styling the Header

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import './Header'
+import './Header.css'
 
 function Header() {
     return (
         <div className="header">
-            <ul>
+            <ul className="header-left">
                 <li className="logo"><a href="/">Book Library</a></li>
             </ul>
 
-            <ul>
+            <ul className="header-right">
                 <li><a href="/">Create</a></li>
                 <li><a href="/">View</a></li>
             </ul>
