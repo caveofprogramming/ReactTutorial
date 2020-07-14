2f9cac8260c62d4038ab8b27c12f285e9d7e3fba
John Purcell <john.purcell@caveofprogramming.com>
2020-07-14 13:19:32 +0200

    03120 Creating a Form

--- client/src/Book.js
+++ client/src/Book.js
@@ -6,7 +6,18 @@ function Book() {
     console.log('server url', process.env.REACT_APP_SERVER_URL);
 
     return (
-        <div id='book-message'>Hello I'm a book!</div>
+        <div>
+            <form>
+                <label htmlFor="author">Author:</label>
+                <input type="text" name="author" id="author" />
+                <label htmlFor="title">Title:</label>
+                <input type="text" name="title" id="title" />
+                <label htmlFor="published">Published:</label>
+                <input type="text" name="published" id="published" />
+                <input type="submit" value="Save" />
+            </form>
+
+        </div>
     );
 }
 
