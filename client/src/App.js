3594266189fbd4f02298bca989c5af207ba3bd3c
John Purcell <john.purcell@caveofprogramming.com>
2020-07-14 12:59:32 +0200

    03110 React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import BookLibrary from './BookLibrary';
+import Book from './Book';
 import Header from './Header';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
 
   return (
-    <div className="App">
-      <Header />
-      <BookLibrary />
-    </div>
+
+    <Router>
+      <div className="App">
+        <Header />
+
+        <Switch>
+          <Route exact path="/">
+            <BookLibrary />
+          </Route>
+          <Route path="/create">
+            <Book />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- client/src/Header.js
+++ client/src/Header.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './Header.css'
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <div className="header">
             <ul className="header-left">
-                <li className="logo"><a href="/">Book Library</a></li>
+                <li className="logo"><Link to="/">Book Library</Link></li>
             </ul>
 
             <ul className="header-right">
-                <li><a href="/">Create</a></li>
-                <li><a href="/">View</a></li>
+                <li><Link to="/create">Create</Link></li>
+                <li><Link to="/">View</Link></li>
             </ul>
         </div>
     );
